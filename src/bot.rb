aaaf40e75e28a21af09730c94424436051e64cf5
pkeoman17@gmail.com <pkeoman17@gmail.com>
2014-02-19 17:06:07 -0800

    reg exp works

--- src/bot.rb
+++ src/bot.rb
@@ -2,7 +2,7 @@
 
 require 'cinch' 
 
-
+#going to be either steam bot(sales), draft bot for hearthstone or dota2 or all
 test = Cinch::Bot.new do 
     configure do |c|
         c.server = "irc.cat.pdx.edu"
@@ -10,12 +10,27 @@ test = Cinch::Bot.new do
         c.nick = "test bot" 
     end
 
-    on :message, "hello" do |m|
-    m.reply "Hello, #{m.user.nick}"
+    on :message, /!hearth (.+ .*), (.+ .*)/ do |m, opt1,opt2|
+      pick = rand(2)
+      if pick == 1 
+            
+        m.reply " #{m.user.nick}: #{opt1}"
+      else
+          m.reply "#{m.user.nick}: #{opt2}"
+      end
+
+  end
+    on :message, /!dota (.+ .*), (.+ .*)/ do |m, opt1,opt2|
+      pick = rand(2)
+      if pick == 1 
+            
+        m.reply " #{m.user.nick}: #{opt1}"
+      else
+          m.reply "#{m.user.nick}: #{opt2}"
+      end
+
   end
 end
 
 test.start
-
-
 
