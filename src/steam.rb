6b7c2a830365482ad8c8a18ffbd746e014a93e22
peter <pkeoman17@gmail.com>
2014-09-09 00:03:50 -0700

    Laying out functions for bot

---
+++ src/steam.rb
@@ -0,0 +1,25 @@
+require 'nokogiri' 
+class Steam 
+    #returns sales for the midweek 
+    def midweek_special()
+       
+    end
+
+       
+
+    #returns sales for the weekend
+    def weekend_special()
+            
+    end
+            
+
+    def daily()
+    end
+     
+
+    def specials()
+        
+        
+    end
+end
+
